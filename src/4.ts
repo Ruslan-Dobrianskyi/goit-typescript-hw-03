e9f93a50e52e015302967fd018cf217c35e4ef47
Ruslan-Dobrianskyi <130309553+Ruslan-Dobrianskyi@users.noreply.github.com>
2024-04-24 19:50:32 +0300

    refactor task 4

--- src/4.ts
+++ src/4.ts
@@ -16,35 +16,33 @@
 
 class Key {
     private signature: number = Math.random();
-    getSignature() {
+    getSignature(): number {
         return this.signature;
     }
 }
 class Person {
+    
     constructor(private key: Key) { }
-    getKey() {
+    getKey(): Key {
         return this.key;
 
     }
 }
 abstract class House { 
-    door: boolean = false;
-    key: Key;
-    tenants: Person[] = [];
-    comeIn(person: Person) {
+    private tenants: Person[] = [];
+    protected door: boolean = false;
+    constructor(protected key: Key) { }
+   
+    comeIn(person: Person): void {
         if (this.door) {
             this.tenants.push(person);
         }
     };
     abstract openDoor(key: Key): void;
 }
-class MyHouse extends House { 
-    constructor(key: Key) {
-        super();
-        this.key = key;
-    }
+class MyHouse extends House {
     openDoor(key: Key): void {
-        if (key === this.key) {
+        if (key.getSignature() === this.key.getSignature()) {
             this.door = true;
         }
     }
